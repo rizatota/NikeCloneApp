801375f1929aefcaa4ed4a8bba2420b9bd68eff3
riza <rizatota182@gmail.com>
2024-04-15 17:10:48 +0200

    create modules ,import and export the js files

---
+++ products.js
@@ -0,0 +1,176 @@
+export const products=[
+    {
+        id:1,
+        img: "images/sh1.png",
+        name: "White t-shirt",
+        price:55,
+        
+    },
+    {
+        id:2,
+        img: "images/k5.png",
+        name: "Black t-shirt",
+        price:60,
+        
+    },
+    {
+        id:3,
+        img: "images/a4.png",
+        name: "Light Blue t-shirt",
+        price:70,
+        
+    },
+    {
+        id:4,
+        img: "images/k1.png",
+        name: "Grey t-shirt",
+        price:60,
+        
+    },
+    {
+        id:5,
+        img: "images/a6.png",
+        name: "Green t-shirt",
+        price:65,
+        
+    },
+    {
+        id:6,
+        img: "images/sh6.png",
+        name: "Darkblue t-shirt",
+        price:80,
+        
+    }
+];
+const kidsProducts=[
+    {
+        id:7,
+        img: "images/k1.png",
+        name: "White t-shirt",
+        price:55,
+        
+    },
+    {
+        id:8,
+        img: "images/k2.png",
+        name: "Black t-shirt",
+        price:60,
+        
+    },
+    {
+        id:9,
+        img: "images/k.png",
+        name: "Light Blue t-shirt",
+        price:70,
+        
+    },
+    {
+        id:10,
+        img: "images/k4.png",
+        name: "Grey t-shirt",
+        price:60,
+        
+    },
+    {
+        id:11,
+        img: "images/k5.png",
+        name: "Green t-shirt",
+        price:65,
+        
+    },
+    {
+        id:12,
+        img: "images/k6.png",
+        name: "Darkblue t-shirt",
+        price:80,
+        
+    }
+];
+const accessoriesProducts=[
+    {
+        id:13,
+        img: "images/a1.png",
+        name: "White t-shirt",
+        price:55,
+        
+    },
+    {
+        id:14,
+        img: "images/a2.png",
+        name: "Black t-shirt",
+        price:60,
+        
+    },
+    {
+        id:15,
+        img: "images/a3.png",
+        name: "Light Blue t-shirt",
+        price:70,
+        
+    },
+    {
+        id:16,
+        img: "images/a4.png",
+        name: "Grey t-shirt",
+        price:60,
+        
+    },
+    {
+        id:17,
+        img: "images/a5.png",
+        name: "Green t-shirt",
+        price:65,
+        
+    },
+    {
+        id:18,
+        img: "images/a6.png",
+        name: "Darkblue t-shirt",
+        price:80,
+        
+    }
+];
+const shoesProducts=[
+    {
+        id:19,
+        img: "images/sh1.png",
+        name: "White t-shirt",
+        price:55,
+        
+    },
+    {
+        id:20,
+        img: "images/sh2.png",
+        name: "Black t-shirt",
+        price:60,
+        
+    },
+    {
+        id:21,
+        img: "images/sh3.png",
+        name: "Light Blue t-shirt",
+        price:70,
+        
+    },
+    {
+        id:22,
+        img: "images/sh4.png",
+        name: "Grey t-shirt",
+        price:60,
+        
+    },
+    {
+        id:23,
+        img: "images/sh5.jpg",
+        name: "Green t-shirt",
+        price:65,
+        
+    },
+    {
+        id:24,
+        img: "images/sh6.png",
+        name: "Darkblue t-shirt",
+        price:80,
+        
+    }
+];
--- script.js
+++ script.js
@@ -1,179 +1,5 @@
-const products=[
-    {
-        id:1,
-        img: "images/sh1.png",
-        name: "White t-shirt",
-        price:55,
-        
-    },
-    {
-        id:2,
-        img: "images/k5.png",
-        name: "Black t-shirt",
-        price:60,
-        
-    },
-    {
-        id:3,
-        img: "images/a4.png",
-        name: "Light Blue t-shirt",
-        price:70,
-        
-    },
-    {
-        id:4,
-        img: "images/k1.png",
-        name: "Grey t-shirt",
-        price:60,
-        
-    },
-    {
-        id:5,
-        img: "images/a6.png",
-        name: "Green t-shirt",
-        price:65,
-        
-    },
-    {
-        id:6,
-        img: "images/sh6.png",
-        name: "Darkblue t-shirt",
-        price:80,
-        
-    }
-];
-const kidsProducts=[
-    {
-        id:7,
-        img: "images/k1.png",
-        name: "White t-shirt",
-        price:55,
-        
-    },
-    {
-        id:8,
-        img: "images/k2.png",
-        name: "Black t-shirt",
-        price:60,
-        
-    },
-    {
-        id:9,
-        img: "images/k.png",
-        name: "Light Blue t-shirt",
-        price:70,
-        
-    },
-    {
-        id:10,
-        img: "images/k4.png",
-        name: "Grey t-shirt",
-        price:60,
-        
-    },
-    {
-        id:11,
-        img: "images/k5.png",
-        name: "Green t-shirt",
-        price:65,
-        
-    },
-    {
-        id:12,
-        img: "images/k6.png",
-        name: "Darkblue t-shirt",
-        price:80,
-        
-    }
-];
-const accessoriesProducts=[
-    {
-        id:13,
-        img: "images/a1.png",
-        name: "White t-shirt",
-        price:55,
-        
-    },
-    {
-        id:14,
-        img: "images/a2.png",
-        name: "Black t-shirt",
-        price:60,
-        
-    },
-    {
-        id:15,
-        img: "images/a3.png",
-        name: "Light Blue t-shirt",
-        price:70,
-        
-    },
-    {
-        id:16,
-        img: "images/a4.png",
-        name: "Grey t-shirt",
-        price:60,
-        
-    },
-    {
-        id:17,
-        img: "images/a5.png",
-        name: "Green t-shirt",
-        price:65,
-        
-    },
-    {
-        id:18,
-        img: "images/a6.png",
-        name: "Darkblue t-shirt",
-        price:80,
-        
-    }
-];
-const shoesProducts=[
-    {
-        id:19,
-        img: "images/sh1.png",
-        name: "White t-shirt",
-        price:55,
-        
-    },
-    {
-        id:20,
-        img: "images/sh2.png",
-        name: "Black t-shirt",
-        price:60,
-        
-    },
-    {
-        id:21,
-        img: "images/sh3.png",
-        name: "Light Blue t-shirt",
-        price:70,
-        
-    },
-    {
-        id:22,
-        img: "images/sh4.png",
-        name: "Grey t-shirt",
-        price:60,
-        
-    },
-    {
-        id:23,
-        img: "images/sh5.jpg",
-        name: "Green t-shirt",
-        price:65,
-        
-    },
-    {
-        id:24,
-        img: "images/sh6.png",
-        name: "Darkblue t-shirt",
-        price:80,
-        
-    }
-];
+import {cart} from './cart.js'
+import { products } from './products.js';
 
 getProducts();
  function getProducts(){
@@ -296,7 +122,15 @@ console.log(shoesContainerDisplay)
                         productId:  productId,
                         quantity: 1,
                     });
-                    console.log(cart);}
+                    console.log(cart);
+                }
+                    let cartQuantity = 0;
+                    cart.forEach((item) =>{
+                        cartQuantity += item.quantity;
+                    });
+                    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+                    console.log(cartQuantity);
+                    
                 
          }
     });
